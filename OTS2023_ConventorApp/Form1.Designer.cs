56ace5567bb908cd3e75ea77d6fff75cca253e2f
MrVolfex <mihailo538@gamil.com>
2024-03-07 19:55:07 +0100

    Vezbe2

--- OTS2023_ConventorApp/Form1.Designer.cs
+++ OTS2023_ConventorApp/Form1.Designer.cs
@@ -28,13 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonMass = new System.Windows.Forms.RadioButton();
+            this.radioButtonLength = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(183, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(183, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(311, 252);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonLength);
+            this.groupBox1.Controls.Add(this.radioButtonMass);
+            this.groupBox1.Location = new System.Drawing.Point(186, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 73);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Converter type";
+            // 
+            // radioButtonMass
+            // 
+            this.radioButtonMass.AutoSize = true;
+            this.radioButtonMass.Location = new System.Drawing.Point(7, 20);
+            this.radioButtonMass.Name = "radioButtonMass";
+            this.radioButtonMass.Size = new System.Drawing.Size(50, 17);
+            this.radioButtonMass.TabIndex = 0;
+            this.radioButtonMass.TabStop = true;
+            this.radioButtonMass.Text = "Mass";
+            this.radioButtonMass.UseVisualStyleBackColor = true;
+            this.radioButtonMass.CheckedChanged += new System.EventHandler(this.radioButtonMass_CheckedChanged);
+            // 
+            // radioButtonLength
+            // 
+            this.radioButtonLength.AutoSize = true;
+            this.radioButtonLength.Location = new System.Drawing.Point(7, 44);
+            this.radioButtonLength.Name = "radioButtonLength";
+            this.radioButtonLength.Size = new System.Drawing.Size(58, 17);
+            this.radioButtonLength.TabIndex = 1;
+            this.radioButtonLength.TabStop = true;
+            this.radioButtonLength.Text = "Length";
+            this.radioButtonLength.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(286, 156);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(286, 185);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonMass;
+        private System.Windows.Forms.RadioButton radioButtonLength;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- OTS2023_ConventorApp/Form1.cs
+++ OTS2023_ConventorApp/Form1.cs
@@ -16,5 +16,40 @@ namespace OTS2023_ConventorApp
         {
             InitializeComponent();
         }
+
+       
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (radioButtonLength.Checked) { 
+                double km=Convert.ToDouble(textBox1.Text)*0.453;
+                textBox2.Text = km.ToString();
+            }
+            else if (radioButtonMass.Checked) {
+                double kg = Convert.ToDouble(textBox1.Text) * 0.305;
+                textBox2.Text = kg.ToString();
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButtonMass_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            if (radioButtonMass.Checked==true) {
+                label1.Text = "punds";
+                label2.Text = "kg";
+            }
+            if (radioButtonLength.Checked == true)
+            {
+                label1.Text = "feet";
+                label2.Text = "meters";
+            }
+
+        }
     }
 }
